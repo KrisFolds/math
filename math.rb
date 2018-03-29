8317736fe8bd865a26f9eb556679cd04f9843b94
Kris Folds <wvkidd1979@gmail.com>
2018-03-29 14:41:13 -0400

    added user input

--- math.rb
+++ math.rb
@@ -1,23 +1,35 @@
-  
-  def Addition(a,b)
-    puts a + b
+def Addition(a,b)
+    puts "Your result is #{a + b}"
 end
 
 def Subtraction (a,b)
-  puts a - b
- 
+  puts "Your result is #{a - b}" 
 end
 
 def Division(a,b)
-  puts a / b
+  puts "Your result is #{a / b}"
  
 end
 
 def Multiplication(a,b)
-  puts a * b
+  puts "Your result is #{a * b}"
  
  end
- Addition(5,6)
- Subtraction(10,5)
- Division(12,6)
- Multiplication(3,4)
+
+puts "Would you like to add, subtract, multiply or divide?"
+answer = gets.chomp
+puts "Please give me the first number:"
+number1 = gets.chomp.to_i
+puts "Please give me the second number:"
+number2 = gets.chomp.to_i
+
+if answer == "add"
+ 	Addition(number1, number2)
+elsif answer == "subtract"
+ 	Subtraction(number1, number2)
+elsif answer == "multiply"
+ 	Multiplication(number1, number2)
+elsif answer == "divide"
+ 	Division(number1, number2)
+end
+
