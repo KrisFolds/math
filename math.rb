16bc5d5cf9609dc1e49edf1cb57bec57830e1f7e
Kris Folds <wvkidd1979@gmail.com>
2018-03-29 14:23:56 -0400

    math.rb file added

---
+++ math.rb
@@ -0,0 +1,24 @@
+  
+  def Addition(a,b)
+    puts a + b
+end
+
+def Subtraction (a,b)
+  puts a - b
+ 
+end
+
+def Division(a,b)
+  puts a / b
+ 
+end
+
+def Multiplication
+  puts a * b
+ 
+ end
+ Addition(5,6)
+ Subtraction(10,5)
+ Division(12,6)
+ Multiplication(3,4)
+ 
